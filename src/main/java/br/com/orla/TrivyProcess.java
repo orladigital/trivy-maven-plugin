fc66cbcf92df1e26a3ef5e401624dc2d34d48bc1
netodevel-orla <jose.neto@orla.tech>
2023-05-01 18:17:28 -0300

    feat: support to other system operations and compressed binaries from upx

--- src/main/java/br/com/orla/TrivyProcess.java
+++ src/main/java/br/com/orla/TrivyProcess.java
@@ -6,7 +6,6 @@ import com.google.common.io.Files;
 import com.google.common.io.Resources;
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import org.codehaus.plexus.util.FileUtils;
 
 public class TrivyProcess extends AbstractOSProcess {
@@ -14,10 +13,13 @@ public class TrivyProcess extends AbstractOSProcess {
     public String getLocationTrivyBin() throws Exception {
         var os = OSDetector.getOS();
         if (os.equals(OS.UNIX)) {
-            URL resourceURL = getClass().getClassLoader().getResource("trivy_UNIX_X86_64");
-            if (resourceURL != null) {
-                return resourceURL.getPath();
-            }
+            return "trivy_UNIX_X86_64";
+        }
+        if (os.equals(OS.WINDOWS)) {
+            return "trivy_WINDOWS_X86_64.exe";
+        }
+        if (os.equals(OS.MAC_OSX)) {
+            return "trivy_MACOS_64";
         }
         throw new Exception("Trivy bin not found");
     }
@@ -36,7 +38,7 @@ public class TrivyProcess extends AbstractOSProcess {
 
     public Integer scanImage(String dockerImageName, String trivyParams) throws Exception {
         var command = "%s image --exit-code 1 %s %s";
-        var bin = extractExecutableFromJar("trivy_UNIX_X86_64");
+        var bin = extractExecutableFromJar(getLocationTrivyBin());
         var finalCommand = String.format(command, bin.getAbsolutePath(), trivyParams, dockerImageName);
         return execProcess(finalCommand, true);
     }
